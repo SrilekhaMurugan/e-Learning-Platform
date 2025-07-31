09ef23ef0665cfce185fb7e36658801aa749dfcc
SrilekhaMurugan <146984979+SrilekhaMurugan@users.noreply.github.com>
2025-07-31 10:13:46 +0530

    Update cloudinary.js

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,8 +1,6 @@
 import {v2 as cloudinary} from 'cloudinary';
 import fs from "fs"
 
-
-
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -25,6 +23,5 @@ const uploadOnCloudinary = async (localFilePath) => {
         return null;
     }
 }
-
 
 export {uploadOnCloudinary}
